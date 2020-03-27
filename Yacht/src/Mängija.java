7b0089b46e1ed03a67b504b260744024af5ccac8
Joonasp1 <56842740+Joonasp1@users.noreply.github.com>
2020-03-27 13:30:56 +0200

    Skoor väli

--- Yacht/src/Mängija.java
+++ Yacht/src/Mängija.java
@@ -1,10 +1,18 @@
+import java.util.ArrayList;
+
 public class Mängija {
     private String nimi;
+    private int skoor;
+
     public String getNimi() {
         return nimi;
     }
 
+    public int getSkoor() {return skoor;}
+
     public Mängija(String nimi) {
         this.nimi = nimi;
     }
+
+
 }
